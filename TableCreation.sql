e118fe59e1c98e28fcbb9ccaa683689fff882aa5
Ahmed <ahmad.alwasifi@gmail.com>
2021-12-11 09:29:41 +0200

    progress in part 6

--- TableCreation.sql
+++ TableCreation.sql
@@ -1,64 +1,7 @@
--- PostGradUser (id, email, password)
--- Admin(id) _ _
--- Where Admin.id references PostGradUser
--- GucianStudent( id,firstName, lastName, type, faculty, address, GPA, undergradID) _ _
--- Where GucianStudent.id references PostGradUser
--- NonGucianStudent( id,firstName, lastName, type,faculty,address,GPA ) _ _
--- Where NonGucianStudent.id references PostGradUser
--- GUCStudentPhoneNumber ( id,phone) _ _
--- Where GUCStudentPhoneNumber.id references GucianStudent
--- NonGUCStudentPhoneNumber ( id,phone) _ _
--- Where NonGUCStudentPhoneNumber.id references NonGucianStudent
--- Course (id, fees,creditHours, code)
--- Supervisor (id, name, faculty) _ _
--- Where Supervisor.id references PostGradUser
--- Thesis (serialNumber, field, type, title, startDate, endDate, defenseDate, years, grade,
--- payment_id, noExtension) _ _ _ _ _ _
--- Where thesis.years = endDate - startDate ,
--- payment_id references Payment
--- Publication (id, title, date, place, accepted, host)
--- Payment (id, amount, no_Installments, fundPercentage)
--- Examiner (id, name, fieldOfWork, isNational ) _ _
--- Where Examiner.id references PostGradUser
--- Defense (serialNumber, date, location, grade) _ _ _ _ _ _ _
--- Where Defense.serial_no references Thesis
--- GUCianProgressReport (sid, no, date, eval, state, thesisSerialNumber, supid) _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
--- Where ProgressReport.sid references GucianStudent
--- , thesisSerialNumber references Thesis,
--- ProgressReport.supid references Supervisor
--- NonGUCianProgressReport (sid, no, date, eval, state, thesisSerialNumber, supid) _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
--- Where ProgressReport.sid references NonGucianStudent
--- , thesisSerialNumber references Thesis,
--- ProgressReport.supid references Supervisor
--- Installment (date, paymentId, amount, done) _ _ _ _ _ _
--- Where paymentId references Payment
--- NonGucianStudentPayForCourse(sid, paymentNo, cid) _ _ _ _ _ _ _ _ _ _
--- Where NonGucianStudentPayForCourse.sid references NonGucianStudent
--- , NonGucianStudentPayForCourse.paymentNo references Payment
--- , NonGucianStudentPayForCourse.cid references Course
--- NonGucianStudentTakeCourse (sid, cid, grade) _ _ _ _
--- Where NonGucianStudentTakeCourse .sid references NonGucianStudent
--- , NonGucianStudentTakeCourse .cid references Course
--- GUCianStudentRegisterThesis (sid, supid, serial_no) _ _ _ _ _ _ _ _ _
--- Where sid references GucianStudent,
--- supid references Supervisor
--- , serialNo references Thesis
--- NonGUCianStudentRegisterThesis (sid, supid, serial_no) _ _ _ _ _ _ _ _ _
--- Where sid references NonGucianStudent,
--- supid references Supervisor
--- , serialNo references Thesis
--- ExaminerEvaluateDefense(date, serialNo, examinerId, comment) _ _ _ _ _ _ _ _ _ _ _ _ _
--- Where date and serialNo references Defense
--- , examinerId references Examiner
--- ThesisHasPublication(serialNo,pubid)
--- _ _ _ _ _ _ _ _
--- Where ThesisHasPublication.serialNo references Thesis,
--- ThesisHasPublication.pubid references Publication
-
 CREATE TABLE PostGradUser (
     id INT PRIMARY KEY IDENTITY,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL
+    email VARCHAR(10) NOT NULL,
+    password VARCHAR(10) NOT NULL
 ); 
 
 CREATE TABLE Admin(
@@ -67,34 +10,34 @@ CREATE TABLE Admin(
 
 CREATE TABLE GucianStudent (
     id INT PRIMARY KEY REFERENCES PostGradUser(id),
-    firstName TEXT NOT NULL,
-    lastName TEXT NOT NULL,
-    type TEXT NOT NULL,
-    faculty TEXT NOT NULL,
-    address TEXT NOT NULL,
+    firstName VARCHAR(10) NOT NULL,
+    lastName VARCHAR(10) NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    faculty VARCHAR(50) NOT NULL,
+    address VARCHAR(10) NOT NULL,
     GPA DECIMAL(3,2) NOT NULL,
     undergradID VARCHAR(10) NOT NULL,
 );
 
 CREATE TABLE NonGucianStudent (
     id INT PRIMARY KEY REFERENCES PostGradUser(id),
-    firstName TEXT NOT NULL,
-    lastName TEXT NOT NULL,
-    type TEXT NOT NULL,
-    faculty TEXT NOT NULL,
-    address TEXT NOT NULL,
+    firstName VARCHAR(10) NOT NULL,
+    lastName VARCHAR(10) NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    faculty VARCHAR(50) NOT NULL,
+    address VARCHAR(10) NOT NULL,
     GPA DECIMAL(3,2) NOT NULL
 );
 
 CREATE TABLE GUCStudentPhoneNumber (
     id INT REFERENCES GucianStudent(id),
-    phone TEXT NOT NULL,
+    phone VARCHAR(50) NOT NULL,
     PRIMARY KEY(id, phone)
 );
 
 CREATE TABLE NonGUCStudentPhoneNumber (
     id INT REFERENCES NonGucianStudent(id),
-    phone TEXT NOT NULL,
+    phone VARCHAR(50) NOT NULL,
     PRIMARY KEY(id, phone)
 );
 
@@ -102,20 +45,35 @@ CREATE TABLE Course (
     id INT PRIMARY KEY IDENTITY,
     fees DECIMAL(5,2) NOT NULL,
     creditHours INT NOT NULL,
-    code TEXT NOT NULL
+    code VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Supervisor (
     id INT PRIMARY KEY REFERENCES PostGradUser(id),
-    name TEXT NOT NULL,
-    faculty TEXT NOT NULL
+    name VARCHAR(50) NOT NULL,
+    faculty VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Payment (
+    id INT PRIMARY KEY IDENTITY,
+    amount DECIMAL(5,2) NOT NULL,
+    no_Installments INT NOT NULL,
+    fundPercentage DECIMAL(3,2) NOT NULL
+);
+
+CREATE TABLE Installment (
+    date DATE NOT NULL,
+    paymentId INT REFERENCES Payment(id),
+    amount DECIMAL(5,2) NOT NULL,
+    done INT NOT NULL,
+    PRIMARY KEY (date, paymentId)
 );
 
 CREATE TABLE Thesis (
     serialNumber INT PRIMARY KEY IDENTITY,
-    field TEXT NOT NULL,
-    type TEXT NOT NULL,
-    title TEXT NOT NULL,
+    field VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
     defenseDate DATE NOT NULL,
@@ -127,31 +85,24 @@ CREATE TABLE Thesis (
 
 CREATE Table Publication(
     id INT PRIMARY KEY IDENTITY,
-    title TEXT NOT NULL,
+    title VARCHAR(50) NOT NULL,
     date DATE NOT NULL,
-    place TEXT NOT NULL,
-    accepted BOOLEAN NOT NULL,
-    host TEXT NOT NULL
-);
-
-CREATE TABLE Payment (
-    id INT PRIMARY KEY IDENTITY,
-    amount DECIMAL(5,2) NOT NULL,
-    no_Installments INT NOT NULL,
-    fundPercentage DECIMAL(3,2) NOT NULL
+    place VARCHAR(50) NOT NULL,
+    accepted BIT NOT NULL,
+    host VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Examiner (
     id INT PRIMARY KEY REFERENCES PostGradUser(id),
-    name TEXT NOT NULL,
-    fieldOfWork TEXT NOT NULL,
-    isNational TEXT NOT NULL
+    name VARCHAR(50) NOT NULL,
+    fieldOfWork VARCHAR(50) NOT NULL,
+    isNational VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Defense (
     serialNumber INT REFERENCES Thesis(serialNumber),
     date DATE NOT NULL,
-    location TEXT NOT NULL,
+    location VARCHAR(50) NOT NULL,
     grade DECIMAL(3,2) NOT NULL,
     PRIMARY KEY(serialNumber, date)
 );
@@ -160,8 +111,8 @@ CREATE TABLE GUCianProgressReport (
     sid INT REFERENCES GucianStudent(id),
     no INT NOT NULL,
     date DATE NOT NULL,
-    eval TEXT NOT NULL,
-    state TEXT NOT NULL,
+    eval VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
     thesisSerialNumber INT REFERENCES Thesis(serialNumber),
     supid INT REFERENCES Supervisor(id),
     PRIMARY KEY (sid, no)
@@ -171,24 +122,16 @@ CREATE TABLE NonGUCianProgressReport (
     sid INT REFERENCES NonGucianStudent(id),
     no INT NOT NULL,
     date DATE NOT NULL,
-    eval TEXT NOT NULL,
-    state TEXT NOT NULL,
+    eval VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
     thesisSerialNumber INT REFERENCES Thesis(serialNumber),
     supid INT REFERENCES Supervisor(id),
     PRIMARY KEY (sid, no)
 );
 
-CREATE TABLE Installment (
-    date DATE NOT NULL,
-    paymentId INT REFERENCES Payment(id),
-    amount DECIMAL(5,2) NOT NULL,
-    done INT NOT NULL,
-    PRIMARY KEY (date, paymentId)
-);
-
 CREATE TABLE NonGucianStudentPayForCourse (
     sid INT REFERENCES NonGucianStudent(id),
-    paymentNo INT NOT NULL,
+    paymentNo INT REFERENCES Payment(id),
     cid INT REFERENCES Course(id),
     PRIMARY KEY (sid, paymentNo, cid)
 );
@@ -218,7 +161,7 @@ CREATE TABLE ExaminerEvaluateDefense (
     date DATE NOT NULL,
     serialNo INT REFERENCES Thesis(serialNumber),
     examinerId INT REFERENCES Examiner(id),
-    comment TEXT NOT NULL,
+    comment VARCHAR(50) NOT NULL,
     PRIMARY KEY (date, serialNo, examinerId)
 );
 
--- procedures.sql
+++ procedures.sql
@@ -1,4 +1,42 @@
-CREATE PROCEDURE SupViewProfile  @supervisorID int
-AS
-SELECT 
+CREATE PROC viewMyProfile (@studentId INT) AS BEGIN
+    SELECT * FROM PostGradUser
+    INNER JOIN GucianStudent ON GucianStudent.id = PostGradUser.id
+    INNER JOIN NonGucianStudent ON NonGucianStudent.id = PostGradUser.id
+    WHERE PostGradUser.id = @studentId;
+END;
 
+GO;
+
+CREATE PROC editMyProfile (@studentId INT, @firstName VARCHAR(10), @lastName VARCHAR(10),
+    @password VARCHAR(10),@email VARCHAR(10), @address VARCHAR(10), @type VARCHAR(10)) AS BEGIN
+    
+    UPDATE PostGradUser SET email = @email, password = @password WHERE id = @studentId;
+    IF EXISTS(SELECT * FROM GucianStudent WHERE id = @studentId)
+        UPDATE GucianStudent SET firstName = @firstName, lastName = @lastName, address = @address, type = @type
+            WHERE id = @studentId;
+    ELSE
+        UPDATE NonGucianStudent SET firstName = @firstName, lastName = @lastName, address = @address, type = @type
+            WHERE id = @studentId;
+END;
+
+GO;
+
+CREATE PROC addUndergradID (@studentId INT, @undergradID VARCHAR(10)) AS BEGIN
+    UPDATE GucianStudent SET undergradID = @undergradID WHERE id = @studentId;
+END;
+
+GO;
+
+CREATE PROC ViewCoursesGrades (@studentId INT) AS BEGIN
+    SELECT Course.code, NonGUCianStudentTakeCourse.grade FROM NonGUCianStudentTakeCourse INNER JOIN Course ON NonGUCianStudentTakeCourse.cid = Course.id WHERE NonGUCianStudentTakeCourse.sid = @studentId;
+END;
+
+GO;
+
+CREATE PROC ViewCoursePaymentsInstall (@studentId INT) AS BEGIN
+    SELECT NonGucianStudentPayForCourse.cid, Installment.* FROM NonGucianStudentPayForCourse
+    INNER JOIN Installment ON Installment.paymentId = NonGucianStudentPayForCourse.paymentNo
+    WHERE NonGucianStudentPayForCourse.sid = @studentId; 
+END;
+
+GO;
